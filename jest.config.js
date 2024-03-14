1d218204858d2ce6e151080fdce42ab6ad40675b
Elijah Okello <elijahokello90@gmail.com>
2024-03-14 16:13:26 +0300

    initial commit and project setup

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["<rootDir>/test/unit/**/*.test.ts"],
+    passWithNoTests: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["**/src/**/*.ts"],
+    coveragePathIgnorePatterns:["dist"],
+    coverageReporters: ["text", ["json", {file: "integration-final.json"}]],
+    coverageDirectory: "./coverage/",
+    clearMocks: true,
+    // coverageThreshold: {
+    //     "global": {
+    //         "branches": 90,
+    //         "functions": 90,
+    //         "lines": 90,
+    //         "statements": -10
+    //     }
+    // }
+}
