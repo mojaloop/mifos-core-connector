938bdace8ed7f45bec72df145fc4f15422fb374d
Elijah Okello <elijahokello90@gmail.com>
2024-03-22 09:40:22 +0300

    created routes class and service class for core connector

---
+++ src/core-connector-svc/Service.ts
@@ -0,0 +1,135 @@
+/*****
+ License
+--------------
+Copyright © 2017 Bill & Melinda Gates Foundation
+The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Contributors
+--------------
+This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+Names of the original copyright holders (individuals or organizations)
+should be listed with a '*' in the first column. People who have
+contributed from an organization can be listed under the organization
+that actually holds the copyright for their contributions (see the
+Gates Foundation organization for an example). Those individuals should have
+their names indented and be marked with a '-'. Email address can be added
+optionally within square brackets <email>.
+
+
+- Okello Ivan Elijah <elijahokello90@gmail.com>
+
+--------------
+******/
+
+"use strict";
+
+import { Server } from "@hapi/hapi";
+import { IHttpClient } from "../domain";
+import { CoreConnectorAggregate } from "../domain";
+import { FineractClient } from "../domain/FineractClient/FineractClient";
+import { AxiosClientFactory } from "../infra/axiosHttpClient";
+import { TFineractConfig } from "../domain/FineractClient/types";
+import { CONFIG } from "./config";
+import { CoreConnectorRoutes } from "./coreConnectorRoutes";
+import { loggerFactory } from "../infra/logger";
+
+const logger = loggerFactory({ context: 'Mifos Core Connector' });
+
+export class Service {
+    static coreConnectorAggregate: CoreConnectorAggregate;
+    static httpClient: IHttpClient;
+    static server: Server;
+
+    static async start(httpClient?: IHttpClient){
+        if(!httpClient){
+            httpClient = AxiosClientFactory.createAxiosClientInstance();
+        }
+        this.httpClient = httpClient;
+        if(
+            !CONFIG.fineractConfig.FINERACT_ACCOUNT_PREFIX || 
+            !CONFIG.fineractConfig.FINERACT_AUTH_MODE ||
+            !CONFIG.fineractConfig.FINERACT_BANK_COUNTRY_CODE ||
+            !CONFIG.fineractConfig.FINERACT_BANK_ID ||
+            !CONFIG.fineractConfig.FINERACT_BASE_URL||
+            !CONFIG.fineractConfig.FINERACT_ID_TYPE ||
+            !CONFIG.fineractConfig.FINERACT_PASSWORD ||
+            !CONFIG.fineractConfig.FINERACT_TENTANT_ID ||
+            !CONFIG.fineractConfig.FINERACT_USERNAME
+            ){
+                throw new Error("Please set the environment variables as used in config.ts through a .env file. Refer to .env.example");
+            }
+        const fineractConfig = CONFIG.fineractConfig as TFineractConfig;
+        const fineractClient = new FineractClient(fineractConfig,this.httpClient, logger);
+        this.coreConnectorAggregate = new CoreConnectorAggregate(fineractConfig,fineractClient,logger);
+
+        await this.setupAndStartUpServer();
+        logger.info("Core Connector Server started");
+    }
+
+    static async setupAndStartUpServer(){
+        return new Promise<void>((resolve)=>{
+            if(!CONFIG.server.PORT || !CONFIG.server.HOST){
+                throw new Error("Please set the environment variables as used in config.ts through a .env file. Refer to .env.example");
+            }
+            this.server = new Server({
+                port: CONFIG.server.PORT,
+                host: CONFIG.server.HOST,
+            });
+
+            const coreConnectorRoutes = new CoreConnectorRoutes(this.coreConnectorAggregate);
+            this.server.route(coreConnectorRoutes.getRoutes());
+            this.server.route({
+                method: '*',
+                path: '/{any*}',
+                handler: function (request, h) {
+                    return h.response('404 Error! Page Not Found!').code(404);
+                },
+            });
+            this.server.start();
+            logger.info(`Core Connector Server running at ${this.server.info.uri}`);
+
+            resolve();
+        });
+    }
+
+    static async stop(){
+        await this.server.stop({timeout: 60});
+        logger.info("Service Stopped");
+    }
+}
+
+// todo: refactor it and move to /src/index.ts
+async function _handle_int_and_term_signals(signal: NodeJS.Signals): Promise<void> {
+    logger.warn(`Service - ${signal} received - cleaning up...`);
+    let clean_exit = false;
+    setTimeout(() => {
+        clean_exit || process.abort();
+    }, 5000);
+
+    // call graceful stop routine
+    await Service.stop();
+
+    clean_exit = true;
+    process.exit();
+}
+
+//catches ctrl+c event
+process.on('SIGINT', _handle_int_and_term_signals.bind(this));
+//catches program termination event
+process.on('SIGTERM', _handle_int_and_term_signals.bind(this));
+
+//do something when app is closing
+/* istanbul ignore next */
+process.on('exit', async () => {
+    logger.info('Service - exiting...');
+});
+
+/* istanbul ignore next */
+process.on('uncaughtException',(err: Error) => {
+    logger.error(`UncaughtException: ${err?.message}`, err);
+    process.exit(999);
+});
