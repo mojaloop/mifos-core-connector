0283c7c09e0bcc908a396d5c4f16826f782932bd
Elijah Okello <elijahokello90@gmail.com>
2024-03-22 20:52:17 +0300

    feat: implemented tests for transfers

--- test/func/index.test.ts
+++ test/func/index.test.ts
@@ -31,7 +31,7 @@ import axios from "axios";
 import { randomUUID } from "crypto";
 import { CONFIG, Service } from "../../src/core-connector-svc";
 import { loggerFactory } from "../../src/infra/logger";
-import { TQuoteRequest } from "../../src/domain/interfaces";
+import { TQuoteRequest, TtransferRequest } from "../../src/domain/interfaces";
 
 
 jest.setTimeout(1000000);
@@ -144,6 +144,126 @@ function extractAccountFromIBAN(IBAN:string): string{
             }
         );
         expect(res.status).toEqual(200);
+    });
+
+    test("POST /transfers Should return transfer if party in fineract", async ()=>{
+      const transfer: TtransferRequest = {
+        "homeR2PTransactionId": "string",
+        "amount": "500",
+        "amountType": "SEND",
+        "currency": "NGN",
+        "from": {
+          "dateOfBirth": "5704-02-29",
+          "displayName": "string",
+          "extensionList": [
+            {
+              "key": "string",
+              "value": "string"
+            }
+          ],
+          "firstName": "string",
+          "fspId": "string",
+          "idSubValue": "string",
+          "idType": "MSISDN",
+          "idValue": "string",
+          "lastName": "string",
+          "merchantClassificationCode": "string",
+          "middleName": "string",
+          "type": "CONSUMER",
+        },
+        "ilpPacket": {
+          "data": {
+            "amount": {
+              "amount": "500",
+              "currency": "NGN"
+            },
+            "payee": {
+              "partyIdInfo":{
+                "partyIdType": "IBAN",
+                "partyIdentifier": IBAN
+              }
+            },
+            "payer": {
+              "partyIdInfo":{
+                "partyIdType": "MSISDN",
+                "partyIdentifier": "820323232"
+              }
+            },
+            "quoteId": "27653e60-e21c-1414-8a35-0b5b97d5abc7",
+            "transactionId": "9fe8c410-5b31-188f-858f-67cb6b308198",
+            "transactionType": {
+              "initiator": "PAYER",
+              "initiatorType": "CONSUMER",
+              "scenario": "TRANSFER",
+              "subScenario": "string"
+            }
+          }
+        },
+        "note": "string",
+        "quote": {
+          "expiration": "5200-02-29T21:42:06.649-09:08",
+          "extensionList": [
+            {
+              "key": "string",
+              "value": "string"
+            }
+          ],
+          "geoCode": {
+            "latitude": "90",
+            "longitude": "+6.06"
+          },
+          "payeeFspCommissionAmount": "0.97",
+          "payeeFspCommissionAmountCurrency": "NGN",
+          "payeeFspFeeAmount": "0",
+          "payeeFspFeeAmountCurrency": "NGN",
+          "payeeReceiveAmount": "0",
+          "payeeReceiveAmountCurrency": "NGN",
+          "quoteId": randomUUID(),
+          "transactionId": randomUUID(),
+          "transferAmount": "500",
+          "transferAmountCurrency": "NGN"
+        },
+        "quoteRequestExtensions": [
+          {
+            "key": "string",
+            "value": "string"
+          }
+        ],
+        "subScenario": "string",
+        "to": {
+          "dateOfBirth": "3956-02-29",
+          "displayName": "string",
+          "extensionList": [
+            {
+              "key": "string",
+              "value": "string"
+            }
+          ],
+          "firstName": "string",
+          "fspId": "string",
+          "idSubValue": "string",
+          "idType": "IBAN",
+          "idValue": IBAN,
+          "lastName": "string",
+          "merchantClassificationCode": "string",
+          "middleName": "string",
+          "type": "CONSUMER",
+        },
+        "transactionType": "TRANSFER",
+        "transferId": randomUUID(),
+        "transactionRequestId": randomUUID()
+      };
+      const url = `${baseurl}/transfers`;
+      const res =  await axios.post(
+        url,
+        JSON.stringify(transfer),
+        {
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+      );
+      expect(res.status).toEqual(201);
     });
  });
 
